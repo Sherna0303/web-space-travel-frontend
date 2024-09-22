e92cf7f5e3892c6b0e384b92c5b25e7f139ab78a
Sebastian Hernandez <sebastianhernandezpatio.2004@gmail.com>
2024-09-21 19:19:24 -0500

    Add cover letter to the page

--- src/app/pages/HomePage/index.tsx
+++ src/app/pages/HomePage/index.tsx
@@ -1,9 +1,10 @@
 import { ReactElement } from "react";
+import { CardHome } from "../../ui/components/CardHome";
 
 export const HomePage = (): ReactElement => {
   return (
     <>
-      
+      <CardHome/>
     </>
   );
 };
