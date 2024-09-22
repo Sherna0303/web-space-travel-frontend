6c8c12f853b446cade1b95de11e4e8d04b97887f
Sebastian Hernandez <sebastianhernandezpatio.2004@gmail.com>
2024-09-21 21:20:47 -0500

    Add FAQ section

--- src/app/pages/HomePage/index.tsx
+++ src/app/pages/HomePage/index.tsx
@@ -1,12 +1,14 @@
 import { ReactElement } from "react";
 import { CardHome } from "../../ui/components/CardHome";
 import Reviews from "../../ui/components/Review";
+import FAQ from "../../ui/components/FAQ";
 
 export const HomePage = (): ReactElement => {
   return (
     <>
       <CardHome/>
       <Reviews/>
+      <FAQ/>
     </>
   );
 };
