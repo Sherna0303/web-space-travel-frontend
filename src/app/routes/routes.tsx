d06e3235760f06d1540fffc52aab9ddfbcc33d4a
Sebastian Hernandez <sebastianhernandezpatio.2004@gmail.com>
2024-09-24 20:58:34 -0500

    Add page for contact, add route in routes file

--- src/app/routes/routes.tsx
+++ src/app/routes/routes.tsx
@@ -1,6 +1,7 @@
 import { HashRouter, Routes, Route, Navigate } from "react-router-dom";
 import MainLayout from "../ui/layouts/layout-main";
 import { HomePage } from "../pages/HomePage";
+import { ContactPage } from "../pages/ContactPage";
 
 export const AppRouter = () => {
   return (
@@ -9,6 +10,7 @@ export const AppRouter = () => {
         <Route path="/" element={<MainLayout />}>
           <Route index element={<Navigate to={"/home"} />} />
           <Route path="home" element={<HomePage />} />
+          <Route path="contact" element={<ContactPage />} />
         </Route>
       </Routes>
     </HashRouter>
