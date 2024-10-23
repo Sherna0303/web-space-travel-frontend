2c01ea824a0d1ac8947bfbd1e2eef91421c4828a
Sebastian Hernandez <sebastianhernandezpatio.2004@gmail.com>
2024-10-22 23:56:18 -0500

    Add login page

---
+++ src/app/pages/LoginPage/index.tsx
@@ -0,0 +1,8 @@
+import { ReactElement } from "react";
+import { FormLogin } from "../../ui/forms/FormLogin";
+
+export const LoginPage = (): ReactElement => {
+  return <>
+    <FormLogin/>
+  </>;
+};
