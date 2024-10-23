94b32c95235f2b7ea18c66aa47adf66977514ac0
Sebastian Hernandez <sebastianhernandezpatio.2004@gmail.com>
2024-10-22 23:53:23 -0500

    Add global styles

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -2,6 +2,7 @@ import { ReactElement } from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { AppRouter } from "./routes/routes";
 import './ui/styles/normalize.css'
+import './ui/styles/global.css'
 
 export const App = (): ReactElement => {
   return <>
