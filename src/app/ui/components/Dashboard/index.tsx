68384b6400f1bf36b9d1e56e0cbe7adbc6485588
Sebastian Hernandez <sebastianhernandezpatio.2004@gmail.com>
2024-10-24 10:55:30 -0500

    Change the location of the admin sidebar

--- src/app/ui/components/Dashboard/index.tsx
+++ src/app/ui/components/Dashboard/index.tsx
@@ -1,13 +1,12 @@
 import { useEffect, useState } from "react";
-import { Link } from "react-router-dom";
 import "./style.css";
-import Icon from "../../elements/Icon/inde";
+
 import { RequestModel } from "../../../core/models/requests.model";
 import { getRequestsService } from "../../../core/services/getRequests.service";
+import { SidebarAdmin } from "../SidebarAdmin";
 
 export const Dashboard = () => {
   const [requests, setRequests] = useState<RequestModel[]>([]);
-  const [sidebarOpen, setSidebarOpen] = useState(true);
 
   useEffect(() => {
     setRequests(getRequestsService());
@@ -17,45 +16,8 @@ export const Dashboard = () => {
 
   return (
     <div className="dashboard-container">
-      <aside
-        className={`sidebar ${sidebarOpen ? "open" : "closed"} position-fixed`}
-      >
-        <button
-          className="toggle-button outline-light"
-          onClick={() => setSidebarOpen(!sidebarOpen)}
-        >
-          {sidebarOpen ? "←" : "→"}
-        </button>
-        <div className="container-links">
-          {sidebarOpen ? (
-            <h3 className="title-sidebar text-center">MoonT Admin</h3>
-          ) : (
-            <Icon size={30} color={"#00bfff"} icon={"rocket-takeoff-fill"} />
-          )}
-          <ul className="nav flex-column">
-            <li className="nav-item">
-              <Link to="/admin/dashboard" className="nav-link-admin">
-                <Icon size={30} color={"#ffff"} icon={"speedometer2"} />
-                <span className={sidebarOpen ? "" : "span-hide"}>
-                  Dashboard
-                </span>
-              </Link>
-            </li>
-            <li className="nav-item">
-              <Link to="requests" className="nav-link-admin">
-                <Icon size={30} color={"#ffff"} icon={"envelope"} />
-                <span className={sidebarOpen ? "" : "span-hide"}>Requests</span>
-              </Link>
-            </li>
-            <li className="nav-item">
-              <Link to="/" className="nav-link-admin">
-                <Icon size={30} color={"#ffff"} icon={"box-arrow-left"} />
-                <span className={sidebarOpen ? "" : "span-hide"}>Exit</span>
-              </Link>
-            </li>
-          </ul>
-        </div>
-      </aside>
+
+      <SidebarAdmin/>
 
       <main className="dashboard-content">
         <h2 className="glow-text">Bienvenido al Dashboard</h2>
